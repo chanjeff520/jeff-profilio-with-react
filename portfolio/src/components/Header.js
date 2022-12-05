7f747514f19cf0a6f60cb91c3ba6a6c634f0ef45
chanjeff520 <chanjeff520@gmail.com>
2022-12-05 03:24:50 -0500

    add header

---
+++ portfolio/src/components/Header.js
@@ -0,0 +1,12 @@
+import react from 'react';
+import '../index.css'
+
+export default function Header() {
+    return (
+        <header>
+            <h1>
+                Jeff Chan
+            </h1>
+        </header>
+    )
+}
